cb6739f82a87c72b43b75fa4aabc03e0eaec2257
Tyler Long <tyler4long@gmail.com>
2015-03-16 19:15:21 +0800

    Update fabric script

--- README.md
+++ README.md
@@ -8,9 +8,8 @@ This repositorty contains the built files of [Khan/KaTeX](https://github.com/Kha
 `bower install katex-build`
 
 
-## For Repo Maintainers
+## For repository maintainers
 
-Update this repo to a newer version: `fab update_to:v0.2.0`
+Update this repo to a newer version: `fab update`
 
 For updating detail, please check `fabfile.py`
-
--- fabfile.py
+++ fabfile.py
@@ -1,10 +1,12 @@
+import requests
 from fabric.api import local
 
-def update_to(version):
+def update():
+    latest_version = requests.get('https://api.github.com/repos/Khan/KaTex/releases/latest').json()['tag_name']
     local('rm -rf fonts/')
     local('rm katex.min.css')
     local('rm katex.min.js')
-    local('wget https://github.com/Khan/KaTeX/releases/download/{0}/katex.zip'.format(version))
+    local('wget https://github.com/Khan/KaTeX/releases/download/{0}/katex.zip'.format(latest_version))
     local('unzip katex.zip')
     local('rm katex.zip')
     local('rm katex/README.md')
