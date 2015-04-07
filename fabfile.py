1f9c3ed4b92c52feea802acf2e5bcb21a974f037
Tyler Long <tyler4long@gmail.com>
2015-04-07 20:43:42 +0800

    v0.3.0

--- fabfile.py
+++ fabfile.py
@@ -1,11 +1,11 @@
 import requests
 from fabric.api import local
 
-
 def update():
     local('rm -rf fonts/')
     local('rm katex.min.css')
     local('rm katex.min.js')
+    local('rm -rf contrib/')
     latest_version = requests.get('https://api.github.com/repos/Khan/KaTex/releases/latest').json()['tag_name']
     local('wget https://github.com/Khan/KaTeX/releases/download/{0}/katex.zip'.format(latest_version))
     local('unzip katex.zip && rm katex.zip')
